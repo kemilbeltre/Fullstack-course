7da94f6afe1bcc387fa0b6c03444f6e3e62b5706
Kemil <kemilbeltre@gmail.com>
2021-04-02 21:03:38 +0100

    Solution exercise 1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,35 +4,46 @@ import React from 'react';
 const Title = ({course}) => <h1>{}
 */
 const Title = ({course}) => <h1>{course}</h1>;
-const Content = (part) => { 
-return (<p>{part.name} {part.exercises}</p>
+const Content = ({parts}) => {
+  return (
+    <div>
+     {parts.map((part) => (
+     <p>{part.name} {part.exercises}</p>
+      ))}
+    </div>
   );
 }
-const Total = (part) => <p>Number of exercise {part.exercises}</p>;
+const Total = ({parts}) => {
+  return (
+    <div>
+     <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    </div>
+  );
+}
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [ 
+    { 
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
-
+]
+}  
   return (
     <div>
-     <Title course = {course} />
-     <Content name = {part1.name} exercise = {part1.exercises}/>
-     <Content name = {part2.name} exercise = {part2.exercises}/>
-     <Content name = {part3.name} exercise = {part3.exercises}/>
-     <Total exercises = {part1.exercises + part2.exercises + part3.exercises}/>
-
+     <Title course = {course.name} />
+     <Content parts={course.parts}/>
+     <Total parts={course.parts}/>
     </div>
   );
 }
