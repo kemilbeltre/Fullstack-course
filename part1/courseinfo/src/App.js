3effbb20984df58c8c157999c0165f35c69b89f4
Kemil <kemilbeltre@gmail.com>
2021-05-10 23:00:23 +0100

    [solve #1 , #2]  Rendering code
    step6 and step7 of the part 2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 /* 
 const Title = ({course}) => <h1>{}
 */
-const Title = ({course}) => <h1>{course}</h1>;
+const Header = ({course}) => <h1>{course}</h1>;
 const Content = ({parts}) => {
   return (
     <div>
@@ -16,32 +16,36 @@ const Content = ({parts}) => {
 const Total = ({parts}) => {
   return (
     <div>
-     <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+     <p><b> Total of {parts[0].exercises + parts[1].exercises + parts[2].exercises} exercises </b></p>
     </div>
   );
 }
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
-    parts: [ 
-    { 
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
-]
-}  
   return (
     <div>
-     <Title course = {course.name} />
+     <Header course = {course.name} />
      <Content parts={course.parts}/>
      <Total parts={course.parts}/>
     </div>
