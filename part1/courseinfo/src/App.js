d592728a97f5f310660d00bdf146f1b0d5a4d23b
Kemil <kemilbeltre@gmail.com>
2021-05-16 00:10:08 +0100

    [Ref: #4] Refactor

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -23,59 +23,59 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const firstCourse = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  };
-
-  const secondCourse = {
-    id: 2,
-    name: "Nodejs",
-    parts: [
-      {
-        name: "Routing",
-        exercises: 3,
-        id: 5,
-      },
-      {
-        name: "Middlewares",
-        exercises: 7,
-        id: 6,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Header course={firstCourse.name} />
-      <Content parts={firstCourse.parts} />
-      <Total parts={firstCourse.parts} />
-
-      <Header course={secondCourse.name} />
-      <Content parts={secondCourse.parts} />
-      <Total parts={secondCourse.parts} />
+      <Header course={courses[0].name} />
+      <Content parts={courses[0].parts} />
+      <Total parts={courses[0].parts} />
+      <Header course={courses[1].name} />
+      <Content parts={courses[1].parts} />
+      <Total parts={courses[1].parts} />
     </div>
   );
 };
