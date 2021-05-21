51cdbb6b7aed65dfee03baf205f9df4c33899848
Kemil <kemilbeltre@gmail.com>
2021-05-21 16:51:00 +0100

    [Ref: #6] The Phonebook Step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+  const [ newName, setNewName ] = useState('');
+
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const nameToAddToState = {
+      name: newName
+    };
+    setPersons(persons.concat(nameToAddToState));
+    setNewName("");
+  
+    console.log("Nombre agregado");
+    console.log(newName);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+         name: <input type="text" onChange={handleChange} value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>{persons.map((person) => {
+        return <p key={person.name}>{person.name}</p>
+    })}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,15 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-
-reportWebVitals();
