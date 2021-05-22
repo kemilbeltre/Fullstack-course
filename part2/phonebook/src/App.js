4d6e30ca76c8f521eddac2a5119e5effd4c3e52c
Kemil <kemilbeltre@gmail.com>
2021-05-22 18:57:37 +0100

    [Ref #9] The Phonebook Step4
    Filter added!

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,19 +2,23 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchContacts, setSearchContacts] = useState("");
 
   const handleChangeName = (event) => {
     setNewName(event.target.value);
   };
   const handleChangeNumber = (event) => {
     setNewNumber(event.target.value);
+  };
+  const handleChangeSearchContacts = (event) => {
+    setSearchContacts(event.target.value);
   };
 
   const handleSubmit = (event) => {
@@ -41,12 +45,24 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={handleChangeSearchContacts}
+          value={searchContacts}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input type="text" onChange={handleChangeName} value={newName} />
+          name:{" "}
+          <input type="text" onChange={handleChangeName} value={newName} />
         </div>
         <div>
-          number: <input type="text" onChange={handleChangeNumber} value={newNumber} />
+          number:{" "}
+          <input type="text" onChange={handleChangeNumber} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -54,7 +70,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => {
+        {persons.filter((person) => {
+          if (searchContacts === "") {
+            return person
+          }else if (person.name.toLowerCase().includes(searchContacts.toLowerCase())) {
+              return person
+          }
+        }).map((person) => {
           return (
             <p key={person.name}>
               {person.name} {person.number}
