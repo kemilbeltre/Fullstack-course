aaf5f8b459f3608c74760290cfac5841d884e05c
Kemil <kemilbeltre@gmail.com>
2021-06-02 18:35:51 +0100

    [Ref: #21] 3.1: Phonebook backend step1
    Simple solution for requirements

--- part3/phonebook/api/src/index.js
+++ part3/phonebook/api/src/index.js
@@ -1,10 +1,37 @@
-const http = require('http')
+const express = require("express");
+const app = express();
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
-})
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.get("/", (request, response) => {
+  response.send("<h1>Welcome to Phonebook</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
