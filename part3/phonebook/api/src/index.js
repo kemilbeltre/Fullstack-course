868c7ab2911dfe15d9d7861c0476feb5151f281d
Kemil <kemilbeltre@gmail.com>
2021-06-03 11:17:25 +0100

    [closed #23] 3.3: Phonebook backend step3

--- part3/phonebook/api/src/index.js
+++ part3/phonebook/api/src/index.js
@@ -32,8 +32,20 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 app.get("/info", (req, res) => {
-  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  );
 });
 
 const PORT = 3001;
