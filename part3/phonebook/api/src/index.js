79c171b3ed881f84f7e131ccdbadec55234d0277
Kemil <kemilbeltre@gmail.com>
2021-06-04 08:29:21 +0100

    [Ref #24] 3.4: Phonebook backend step4

--- part3/phonebook/api/src/index.js
+++ part3/phonebook/api/src/index.js
@@ -42,6 +42,12 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
 app.get("/info", (req, res) => {
   res.send(
     `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
