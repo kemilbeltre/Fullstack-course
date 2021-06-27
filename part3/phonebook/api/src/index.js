4986e62e1a6a6c08e9f2b6019e370030035daa0b
Kemil <kemilbeltre@gmail.com>
2021-06-27 19:22:35 +0100

    Phonebook database step 1, 2 (#40 #41)

--- part3/phonebook/api/src/index.js
+++ part3/phonebook/api/src/index.js
@@ -1,117 +1,87 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const Person = require('./models/person');
 
 const app = express();
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static('build'));
 
 app.use(express.json());
 
-morgan.token("post", (req, res) => JSON.stringify(req.body));
+morgan.token('post', (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(function (tokens, req, res) {
-    if (tokens.method(req, res) === "POST") {
+    if (tokens.method(req, res) === 'POST') {
       return [
         tokens.method(req, res),
         tokens.url(req, res),
         tokens.status(req, res),
-        tokens.res(req, res, "content-length"),
-        "-",
-        tokens["response-time"](req, res),
-        "ms",
-        tokens.post(req, res),
-      ].join(" ");
+        tokens.res(req, res, 'content-length'),
+        '-',
+        tokens['response-time'](req, res),
+        'ms',
+        tokens.post(req, res)
+      ].join(' ');
     } else {
       return [
         tokens.method(req, res),
         tokens.url(req, res),
         tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms'
-      ].join(' ')
+        tokens.res(req, res, 'content-length'),
+        '-',
+        tokens['response-time'](req, res),
+        'ms'
+      ].join(' ');
     }
   })
 );
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Phonebook</h1>");
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
-});
-
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
-app.post("/api/persons", (request, response) => {
+app.post('/api/persons', (request, response) => {
   const { name, number } = request.body;
 
   if (!name || !number) {
     return response.status(400).json({
-      error: "content missing",
+      error: 'content missing'
     });
   }
 
-  person = {
-    name,
-    number,
-    id: generateId(),
-  };
+  const person = new Person({
+    name: name,
+    number: number
+  });
 
-  persons = persons.concat(person);
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter((person) => person.id !== id);
 
   res.status(204).end();
 });
 
-app.get("/info", (req, res) => {
+app.get('/info', (req, res) => {
   res.send(
-    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+    `<p>Phonebook has info for ${Person.length} people</p><p>${new Date()}</p>`
   );
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
