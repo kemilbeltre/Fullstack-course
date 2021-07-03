78d7703534807a5a6546512bbd4554baae0a0402
Kemil <kemilbeltre@gmail.com>
2021-07-03 23:22:01 +0100

    Moving error handling into middleware (#44)

--- part3/phonebook/api/src/index.js
+++ part3/phonebook/api/src/index.js
@@ -5,9 +5,9 @@ const cors = require('cors');
 const Person = require('./models/person');
 
 const app = express();
+
 app.use(cors());
 app.use(express.static('build'));
-
 app.use(express.json());
 
 morgan.token('post', (req, res) => JSON.stringify(req.body));
@@ -44,19 +44,12 @@ app.get('/api/persons', (request, response) => {
   });
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-    .then((person) => {
-      if (person) {
-        response.json(person);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: 'malformatted id' });
-    });
+    .then((person) =>
+      person ? response.json(person) : response.status(404).end()
+    )
+    .catch((error) => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -90,6 +83,24 @@ app.get('/info', (req, res) => {
     `<p>Phonebook has info for ${Person.length} people</p><p>${new Date()}</p>`
   );
 });
+
+const unknownEndpoint = (request, response, next) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+  next(error)
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT;
 app.listen(PORT);
