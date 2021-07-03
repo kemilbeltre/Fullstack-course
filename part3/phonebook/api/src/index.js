624c87e3fa720707bd132c00ba1d01a1c509c86f
Kemil <kemilbeltre@gmail.com>
2021-07-03 22:09:17 +0100

    Adding the appropriate status code and some data to the response (#46)

--- part3/phonebook/api/src/index.js
+++ part3/phonebook/api/src/index.js
@@ -55,7 +55,7 @@ app.get('/api/persons/:id', (request, response) => {
     })
     .catch((error) => {
       console.log(error);
-      response.status(500).end();
+      response.status(400).send({ error: 'malformatted id' });
     });
 });
 
