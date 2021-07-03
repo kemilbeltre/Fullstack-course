740106da93feaf41cf1b0e97aa32cb9d8c44e42b
Kemil <kemilbeltre@gmail.com>
2021-07-03 22:03:16 +0100

    Ref: Phonebook database step6 (#46)

--- part3/phonebook/api/src/index.js
+++ part3/phonebook/api/src/index.js
@@ -45,9 +45,18 @@ app.get('/api/persons', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(500).end();
+    });
 });
 
 app.post('/api/persons', (request, response) => {
