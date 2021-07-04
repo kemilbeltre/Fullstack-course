cf9c342cd1b6a91c6706b9c2093620c7453f7de7
Kemil <kemilbeltre@gmail.com>
2021-07-04 12:18:08 +0100

    deleteName fixe (#43)

--- part3/phonebook/webapp/src/App.js
+++ part3/phonebook/webapp/src/App.js
@@ -1,18 +1,18 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect } from 'react';
 
-import { Persons } from "./Components/Persons";
-import Notification from "./Components/Notification";
-import Filter from "./Components/Filter";
-import PersonForm from "./Components/PersonForm";
-import PersonService from "./Services/Person.service";
+import { Persons } from './Components/Persons';
+import Notification from './Components/Notification';
+import Filter from './Components/Filter';
+import PersonForm from './Components/PersonForm';
+import PersonService from './Services/Person.service';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [filter, setFilter] = useState("");
-  const [message, setMessage] = useState("");
-  const [messageType, setMessageType] = useState("");
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+  const [message, setMessage] = useState('');
+  const [messageType, setMessageType] = useState('');
 
   useEffect(() => {
     PersonService.getAll().then((response) => {
@@ -20,28 +20,35 @@ const App = () => {
     });
   }, []);
 
-  const deletePerson = (event) => {
+  const deleteName = (event) => {
     event.preventDefault();
-    const id = parseInt(event.target.value);
-    const name = persons[id - 1].name;
-    PersonService.remove(persons[id - 1]).catch((error) => {
-      setMessageType("error");
-      setMessage(`Information of ${name} was already removed from server`);
-      setTimeout(() => {
-        setMessage(null);
-        setMessageType("error");
-      }, 5000);
+    const personToBeRemoved = persons.find(
+      (person) => person.id === event.target.value
+    );
+    const id = personToBeRemoved.id;
+    const name = personToBeRemoved.name;
+    if (window.confirm(`Delete ${name} ?`)) {
+      PersonService.remove(id).catch((error) => {
+        setMessageType('error');
+        setMessage(
+          `Information of ${name} has already been removed from server`
+        );
+        setTimeout(() => {
+          setMessage(null);
+          setMessageType('error');
+        }, 5000);
+        setPersons(persons.filter((n) => n.id !== id));
+      });
       setPersons(persons.filter((n) => n.id !== id));
-    });
-    setPersons(persons.filter((n) => n.id !== id));
+    }
   };
 
   const postPerson = (newObject) => {
     PersonService.post(newObject).then((response) => {
       setPersons(persons.concat(response.data));
-      setNewName("");
-      setNewNumber("");
-      setMessageType("confirmation");
+      setNewName('');
+      setNewNumber('');
+      setMessageType('confirmation');
       setMessage(`Added ${newName}`);
       setTimeout(() => {
         setMessage(null);
@@ -63,8 +70,8 @@ const App = () => {
           )
         );
       }
-      setNewName("");
-      setNewNumber("");
+      setNewName('');
+      setNewNumber('');
     });
     return;
   };
@@ -77,12 +84,12 @@ const App = () => {
     event.preventDefault();
     const dataToAddToState = {
       name: newName,
-      number: newNumber,
+      number: newNumber
     };
     const personCheck = persons.find((person) => person.name === newName);
 
     if (
-      typeof personCheck !== "undefined" &&
+      typeof personCheck !== 'undefined' &&
       personCheck.number !== newNumber
     ) {
       updatePerson(personCheck, dataToAddToState, newName);
@@ -111,7 +118,7 @@ const App = () => {
 
       <h2>Numbers</h2>
 
-      <Persons persons={persons} filter={filter} deletePerson={deletePerson} />
+      <Persons persons={persons} filter={filter} deleteName={deleteName} />
     </div>
   );
 };
