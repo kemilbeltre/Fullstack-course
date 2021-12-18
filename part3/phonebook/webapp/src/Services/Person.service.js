26e9294c410dca5ae05b2b74f1862d44ec5aa33d
K <kemilbeltre@gmail.com>
2021-12-18 13:43:43 +0000

    Fix person service

--- part3/phonebook/webapp/src/Services/Person.service.js
+++ part3/phonebook/webapp/src/Services/Person.service.js
@@ -3,23 +3,24 @@ import axios from 'axios';
 const baseUrl = '/api/persons';
 
 const getAll = async () => {
-  const request = await axios.get(baseUrl);
-  return request.then((response) => response.data);
+  return axios.get(baseUrl);
 };
 
-const post = async (personObject) => {
-  const request = await axios.post(baseUrl, personObject);
-  return request.then((response) => response.data);
+const post = (personObject) => {
+  return axios.post(baseUrl, personObject);
 };
+
 
 const update = async (id, newObject) => {
-  const request = await axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
+  return axios
+      .put(`${baseUrl}/${id}`, newObject)
+      .then((response) => response.data);
 };
 
 const remove = async (id) => {
-  const request = await axios.delete(`${baseUrl}/${id}`)
-  return request.then((response) => response.data);
+  return axios
+      .delete(`${baseUrl}/${id}`)
+      .then((response) => response.data);
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
